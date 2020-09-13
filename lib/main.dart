042beb0e8bfdc12c068078e9797c9f339910b2ff
enamulhaque <enamulhaque028@gmail.com>
2020-09-13 23:16:45 +0600

    Implemented google map with marker

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Marker> myMarker = [];
+
+  @override
+  void initState() {
+    super.initState();
+    myMarker.add(
+      Marker(
+        markerId: MarkerId('myMarker'),
+        draggable: false,
+        position: LatLng(23.761694, 90.419091),
+        onTap: () => print('Marker Tapped'),
+      ),
+    );
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Google Maps'),
+      ),
+      body: Center(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: GoogleMap(
+            initialCameraPosition: CameraPosition(
+              target: LatLng(23.761694, 90.419091),
+              zoom: 14.0,
+            ),
+            mapType: MapType.normal,
+            markers: Set.from(myMarker),
+          ),
+        ),
+      ),
+    );
+  }
+}
